38d9e1b3c52bb702366e2cbf60ed56d6861a10ea
Bugsia <felix.weihermann@web.de>
2025-01-14 19:00:55 +0100

    Add project files.

---
+++ Terraining.cpp
@@ -0,0 +1,12 @@
+﻿// Terraining.cpp : Defines the entry point for the application.
+//
+
+#include "Terraining.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
