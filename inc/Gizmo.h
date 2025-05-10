742435e90632f53e64051a08b2951bfeb4848ca1
Bugsia <felix.weihermann@web.de>
2025-05-10 22:39:00 +0200

    using pointers instead of references in gizmo

--- inc/Gizmo.h
+++ inc/Gizmo.h
@@ -7,15 +7,15 @@
 
 class Gizmo : Entity<Vector3>, Drawable, ModelObject, MouseCollider {
 public:
-	Gizmo(Vector3& objPosition);
-	Gizmo(Vector3& objPosition, std::string arrowXPath, std::string arrowYPath, std::string arrowZPath);
+	Gizmo(Vector3* objPosition);
+	Gizmo(Vector3* objPosition, std::string arrowXPath, std::string arrowYPath, std::string arrowZPath);
 
 	void draw();
 	void update(int targetFPS, const Camera& camera);
 	void checkCollision(Ray mouseRay);
 
 private:
-	Vector3& m_objPosition;
+	Vector3* m_objPosition;
 	char m_hit = 0; // 0 = no hit, 1 = x hit, 2 = y hit, 3 = z hit
 	RayCollision m_mouseCollision = { 0 };
 	RayCollision m_prevMouseCollision = { 0 };
--- src/Gizmo.cpp
+++ src/Gizmo.cpp
@@ -1,10 +1,10 @@
 #include "Gizmo.h"
 #include <raymath.h>
 
-Gizmo::Gizmo(Vector3& objPosition) : Gizmo(objPosition, "data/models/arrowX.obj", "data/models/arrowY.obj", "data/models/arrowZ.obj") {
+Gizmo::Gizmo(Vector3* objPosition) : Gizmo(objPosition, "data/models/arrowX.obj", "data/models/arrowY.obj", "data/models/arrowZ.obj") {
 }
 
-Gizmo::Gizmo(Vector3& objPosition, std::string arrowXPath, std::string arrowYPath, std::string arrowZPath) : m_objPosition(objPosition) {
+Gizmo::Gizmo(Vector3* objPosition, std::string arrowXPath, std::string arrowYPath, std::string arrowZPath) : m_objPosition(objPosition) {
 	// Load arrow mesh
 	Mesh arrowX = getMeshFromModel(LoadModel(arrowXPath.c_str()), 0);
 	Mesh arrowY = getMeshFromModel(LoadModel(arrowYPath.c_str()), 0);
@@ -64,15 +64,15 @@ void Gizmo::update(int targetFPS, const Camera& camera) {
 		switch (m_hit) {
 		case 1:
 			m_position.x += distance;
-			m_objPosition.x += distance;
+			m_objPosition->x += distance;
 			break;
 		case 2:
 			m_position.y += distance;
-			m_objPosition.y += distance;
+			m_objPosition->y += distance;
 			break;
 		case 3:
 			m_position.z += distance;
-			m_objPosition.z += distance;
+			m_objPosition->z += distance;
 			break;
 		}
 	}
